32ba704f1cd1cc2412501b114dcb68b5f745eb13
STRRL <str_ruiling@outlook.com>
2022-04-06 23:39:06 +0800

    chore: completion for commands
    Signed-off-by: STRRL <str_ruiling@outlook.com>

--- pkg/dodo/cmd/committee-member-add.go
+++ pkg/dodo/cmd/committee-member-add.go
@@ -49,14 +49,40 @@ dodo committee member --committee-name dodo-says --public-key ./alice.pub --desc
 			}
 			return nil
 		},
+		ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+			return nil, cobra.ShellCompDirectiveNoFileComp
+		},
 	}
 
 	cmd.Flags().StringVar(&options.CommitteeName, "committee-name", "", "The name of the committee to add the member to")
 	cmd.Flags().StringVarP(&options.Description, "description", "d", "", "The description of the member")
 	cmd.Flags().StringVar(&options.PublicKeyFilePath, "public-key", "", "The public key of the member")
 
-	_ = cmd.MarkFlagRequired("committee-name")
-	_ = cmd.MarkFlagRequired("public-key")
+	err := cmd.MarkFlagRequired("committee-name")
+	if err != nil {
+		return nil, errors.Wrapf(err, "within command %s, mark flag %s required", cmd.Name(), "committee-name")
+	}
+	err = cmd.MarkFlagRequired("public-key")
+	if err != nil {
+		return nil, errors.Wrapf(err, "within command %s, mark flag %s required", cmd.Name(), "public-key")
+	}
+
+	err = cmd.RegisterFlagCompletionFunc("committee-name", func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		ctx := context.TODO()
+		committeeService := BootstrapCommitteeService(globalOptions.StorageDir)
+		committees, err := committeeService.ListCommittee(ctx)
+		if err != nil {
+			return nil, cobra.ShellCompDirectiveError
+		}
+		var committeeNames []string
+		for _, item := range committees {
+			committeeNames = append(committeeNames, item.Name)
+		}
+		return committeeNames, cobra.ShellCompDirectiveNoFileComp
+	})
+	if err != nil {
+		return nil, errors.Wrapf(err, "within command %s, register flag completion for %s", cmd.Name(), "committee-name")
+	}
 
 	return cmd, nil
 }
--- pkg/dodo/cmd/committee-member-list.go
+++ pkg/dodo/cmd/committee-member-list.go
@@ -12,6 +12,11 @@ func NewCommitteeMemberListCommand(globalOptions *GlobalOptions) (*cobra.Command
 	var cmd = &cobra.Command{
 		Use:   "list",
 		Short: "List committee members of a committee",
+		Example: `
+# List committee members in committee dodo
+dodo committee-member list --committee-name dodo
+`,
+		Args: cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			ctx := context.TODO()
 			committeeService := BootstrapCommitteeService(globalOptions.StorageDir)
@@ -24,13 +29,35 @@ func NewCommitteeMemberListCommand(globalOptions *GlobalOptions) (*cobra.Command
 			}
 			return nil
 		},
+		ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+			return nil, cobra.ShellCompDirectiveNoFileComp
+		},
 	}
 
 	cmd.Flags().StringVar(&options.CommitteeName, "committee-name", "", "Committee name")
+
 	err := cmd.MarkFlagRequired("committee-name")
 	if err != nil {
-		return nil, errors.Wrapf(err, "within command %s, mark flag %s as required", "list", "committee-name")
+		return nil, errors.Wrapf(err, "within command %s, mark flag %s as required", cmd.Name(), "committee-name")
 	}
+
+	err = cmd.RegisterFlagCompletionFunc("committee-name", func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		ctx := context.TODO()
+		committeeService := BootstrapCommitteeService(globalOptions.StorageDir)
+		committees, err := committeeService.ListCommittee(ctx)
+		if err != nil {
+			return nil, cobra.ShellCompDirectiveError
+		}
+		var committeeNames []string
+		for _, item := range committees {
+			committeeNames = append(committeeNames, item.Name)
+		}
+		return committeeNames, cobra.ShellCompDirectiveNoFileComp
+	})
+	if err != nil {
+		return nil, errors.Wrapf(err, "within command %s, register flag completion for %s", cmd.Name(), "committee-name")
+	}
+
 	return cmd, nil
 }
 
